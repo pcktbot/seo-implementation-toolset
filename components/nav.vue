04c4738fd30a62319f76e8cfd7aba7e8ea7e50b8
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-01-23 13:06:43 -0800

    add nav and selects

---
+++ components/nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-nav
+  class=" navbar-expand-lg  bg-light fixed-top">
+    <b-container>
+      <a href="">
+        <b-img
+          class="logo"
+          src="/g5-logo.png"
+          alt="Responsive image"
+        />
+      </a>
+      <h4 class="title">
+        SEO Implementation Toolset
+      </h4>
+      <b-container />
+    </b-container>
+  </b-nav>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.logo {
+    width: 3em;
+    height: 3em;
+}
+h4.title {
+  margin-bottom: 0px
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,57 @@
 <template>
-  <b-container fluid>
-    Content
-    <form-stepper />
-  </b-container>
+  <div>
+    <g5Nav />
+    <b-container fluid>
+      Content
+      <form-stepper />
+      <b-row>
+        <b-col v-for="select in selects" :key="select.selected">
+          <b-form-select v-model="select.selected" :options="select.options" />
+        </b-col>
+      </b-row>
+      <!-- <div class="mt-3">Selected: <strong>{{ verticals.selected }}</strong></div> -->
+    </b-container>
+  </div>
 </template>
 
 <script>
 import FormStepper from '~/components/form-stepper'
-
+import g5Nav from '~/components/nav'
 export default {
   components: {
-    FormStepper
+    FormStepper,
+    g5Nav
+  },
+  data () {
+    return {
+      selects: {
+        verticals: {
+          selected: null,
+          options: [
+            { value: null, text: 'Select Vertical' },
+            { value: 'mf', text: 'Multi-Family' },
+            { value: 'ss', text: 'Self Storage' },
+            { value: 'sl', text: 'Senior Living' }
+          ]
+        },
+        domain: {
+          selected: null,
+          options: [
+            { value: null, text: 'Select Domain Strategy' },
+            { value: 'multi', text: 'Multi Domain' },
+            { value: 'single', text: 'Single Domain' }
+          ]
+        },
+        branding: {
+          selected: null,
+          options: [
+            { value: null, text: 'Select Chain Branding' },
+            { value: 'yes', text: 'Yes' },
+            { value: 'no', text: 'No' }
+          ]
+        }
+      }
+    }
   }
 }
 </script>
