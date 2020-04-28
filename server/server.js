df55e5f683a1ed16e00e881ae3b3c35ed35ed891
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-04-28 11:40:50 -0700

    update greenlock setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 require('dotenv').config()
 const { Nuxt, Builder } = require('nuxt')
 const config = require('../nuxt.config.js')
+const express = require('express')
+const bodyParser = require('body-parser')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+const config = require('../nuxt.config.js')
 const models = require('./models')
+app.use(bodyParser.json({ limit: '10000kb' }))
+require('./routes/api')(app)
+require('./routes/placesapi')(app)
+require('./routes/uspsapi')(app)
 
 config.dev = !(process.env.NODE_ENV === 'production')
 
@@ -13,7 +23,6 @@ require('greenlock-express')
     }
   })
   .ready(async (glx) => {
-    const app = require('./express.js')
     const nuxt = new Nuxt(config)
 
     if (config.dev) {
