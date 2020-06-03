3e91000c428c67874ca4f782d98b2f32e4ef4976
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-06-03 11:11:53 -0700

    add whitelist

--- server/index.js
+++ server/index.js
@@ -30,10 +30,22 @@ const authConfig = {
   },
   sucessRedirectPath: '/'
 }
+const regexWhitelist = []
 g5Auth.init(app, authConfig)
 
 app.use(bodyParser.json({ limit: '10000kb' }))
-app.use(g5Auth.isAuthenticated)
+app.use(checkWhiteList)
+function checkWhiteList(req, res, next) {
+  if (/^(\/_nuxt\/manifest\.[0-9a-z]*\.json).*$/.test(req.path) || dynamicWhitelist(req.path)) {
+    next()
+  } else {
+    g5Auth.isAuthenticated(req, res, next)
+  }
+}
+
+function dynamicWhitelist(path) {
+  return regexWhitelist.some(url => url.test(path))
+}
 require('./routes/api')(app)
 require('./routes/placesapi')(app)
 require('./routes/uspsapi')(app)
