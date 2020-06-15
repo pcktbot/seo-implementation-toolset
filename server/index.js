c9ea1374d5ecabd419717c56f492e31d91ff1423
Colin McCullough <colin.n.mccullough@gmail.com>
2020-06-15 13:46:57 -0700

    test

--- server/index.js
+++ server/index.js
@@ -52,6 +52,8 @@ function checkWhiteList(req, res, next) {
 }
 
 function dynamicWhitelist(path) {
+  // eslint-disable-next-line no-console
+  console.log(path)
   return regexWhitelist.some(url => url.test(path))
 }
 require('./routes/api')(app)
