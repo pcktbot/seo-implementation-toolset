3d51160ba49f17ea5d42b3b9a85d85c5303593b6
Colin McCullough <colin.n.mccullough@gmail.com>
2020-06-15 14:06:40 -0700

    whitelist

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const config = require('../nuxt.config.js')
 const express = require('express')
 const bodyParser = require('body-parser')
 const consola = require('consola')
 const g5Auth = require('@getg5/g5-auth')
 const { Nuxt, Builder } = require('nuxt')
+const config = require('../nuxt.config.js')
 const app = express()
 const models = require('./models')
 const {
@@ -30,9 +30,32 @@ const authConfig = {
   },
   sucessRedirectPath: '/'
 }
+
+const regexWhitelist = [
+  /\/copy\/\d*$/,
+  /\/api\/lp-project\/\d*$/,
+  /\/api\/locations\/\d*$/,
+  /\/_nuxt\/\S*\.js$/,
+  /\/g5-logo-2\.png/,
+  /\/green-check\.svg/,
+  /\/red-x\.svg/
+]
+
 g5Auth.init(app, authConfig)
 app.use(bodyParser.json({ limit: '10000kb' }))
-app.use(g5Auth.isAuthenticated)
+app.use(checkWhiteList)
+function checkWhiteList(req, res, next) {
+  if (dynamicWhitelist(req.path)) {
+    next()
+  } else {
+    g5Auth.isAuthenticated(req, res, next)
+  }
+}
+function dynamicWhitelist(path) {
+  // eslint-disable-next-line no-console
+  console.log(path)
+  return regexWhitelist.some(url => url.test(path))
+}
 require('./routes/api')(app)
 require('./routes/placesapi')(app)
 require('./routes/uspsapi')(app)
