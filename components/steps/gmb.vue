0ebca707251b93fdee19fad87c18bcd099162ea7
Colin McCullough <colin.n.mccullough@gmail.com>
2020-06-23 14:19:00 -0700

    test

--- components/steps/gmb.vue
+++ components/steps/gmb.vue
@@ -132,6 +132,10 @@ export default {
       return properties.gmb && properties.ga && this.strategy
     },
     pickOptions(index) {
+      // eslint-disable-next-line no-console
+      console.log(index)
+      // eslint-disable-next-line no-console
+      console.log(this.vertical)
       return index === 2
         ? this.gmbStore.selects[index][`${this.vertical}options`]
         : this.gmbStore.selects[index].options
